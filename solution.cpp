273e0ead020353a6da2f39c9a813e7fa46ed2fe7
Benson <bensonlin321@gmail.com>
2020-10-13 21:19:35 +0800

    update comment
    update comment

--- solution.cpp
+++ solution.cpp
@@ -12,7 +12,7 @@
 #include <algorithm>
 
 /*
-Input: nums = [1,2,5,1,1,10,1,1,1]
+Input: nums = [1,2,3,1]
 Output: 4
 Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
              Total amount you can rob = 1 + 3 = 4.
